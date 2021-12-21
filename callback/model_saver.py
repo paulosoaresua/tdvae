99ba06ed903afdc6b4cbe0804280c90ccbfcfb45
Paulo Soares <paulosoares@SBS-7186.local>
2021-12-21 08:52:32 -0700

    Saving model per epoch and fixing DataLoader random state persistance

--- callback/model_saver.py
+++ callback/model_saver.py
@@ -8,30 +8,27 @@ from model import BaseModel
 
 class ModelSaver(Callback):
 
-    def __init__(self, model: BaseModel, optimizer: torch.optim, out_dir: str, update_frequency_type: str = 'batch',
-                 update_frequency: int = 100):
-        super().__init__(update_frequency_type, update_frequency)
+    def __init__(self, model: BaseModel, optimizer: torch.optim, out_dir: str, update_frequency: int = 100):
+        # Model saving is fixed per epoch
+        super().__init__('epoch', update_frequency)
 
         self._model = model
         self._optimizer = optimizer
         self._out_dir = out_dir
 
     def _on_train_batch_end(self, batch: int, logs: Dict[str, Any], train: bool):
-        self._save_model(logs)
+        self._save_model()
 
     def _on_train_epoch_end(self, epoch: int, logs: Dict[str, Any], train: bool):
-        self._save_model(logs)
+        self._save_model()
 
-    def _save_model(self, logs: Dict[str, Any]):
+    def _save_model(self):
         data_package = {'random_state': random.getstate(),
                         'numpy_random_state': np.random.get_state(),
                         'torch_random_state': torch.get_rng_state(),
-                        # 'last_batch_torch_random_state': logs['last_batch_torch_random_state'],
                         'model_state_dict': self._model.state_dict(),
                         'optimizer_state_dict': self._optimizer.state_dict(),
-                        'epoch': self._epoch,
-                        'batch': self._batch,
-                        'num_batches': self._num_batches}
+                        'epoch': self._epoch}
 
         order = str(int(self._step))
         filename = f"{self._out_dir}/model.{order}.pt"
--- runner/model_runner.py
+++ runner/model_runner.py
@@ -4,25 +4,8 @@ from model import BaseModel
 from typing import List
 from callback import Callback
 from torch.utils.data import Dataset, DataLoader
-from torch.utils.data.dataloader import Sampler
 import random
 import numpy as np
-
-
-class IndexedDataLoaderSampler(Sampler):
-    """
-    Sampler for DataLoader such that the order of the batches can be persisted and retrieved
-    when training the model from its last state
-    """
-
-    def __init__(self, data_size: int, batch_size: int, batch=0):
-        self.seq = list(range(data_size))[batch * batch_size:]
-
-    def __iter__(self):
-        return iter(self.seq)
-
-    def __len__(self):
-        return len(self.seq)
 
 
 class ModelRunner:
@@ -51,58 +34,41 @@ class ModelRunner:
 
         # In the first iteration of training, these will be used to preserve the last random states
         # at the time of the model was saved
-        self._random_state_initializers = {
-            'random_state': data_package['random_state'],
-            'numpy_random_state': data_package['numpy_random_state'],
-            'torch_random_state': data_package['torch_random_state'],
-            # 'last_batch_torch_random_state': data_package['last_batch_torch_random_state']
-        }
-
-        last_epoch = data_package['epoch']
-        last_batch = data_package['batch']
-        num_batches = data_package['num_batches']
-
-        if last_batch == num_batches - 1:
-            print("yes")
-            self._initial_epoch = last_epoch + 1
-            self._initial_batch = 0
-        else:
-            self._initial_batch = last_batch + 1
-            self._initial_epoch = last_epoch
+        # self._random_state_initializers = {
+        #     'random_state': data_package['random_state'],
+        #     'numpy_random_state': data_package['numpy_random_state'],
+        #     'torch_random_state': data_package['torch_random_state'],
+        # }
+        random.setstate(data_package['random_state'])
+        np.random.set_state(data_package['numpy_random_state'])
+        torch.set_rng_state(data_package['torch_random_state'])
+        self._initial_epoch = data_package['epoch'] + 1
 
     def train(self, training_set: Dataset, epochs: int, batch_size: int, callbacks: List[Callback]):
         self._model.train()
         self._model.stop_training = False
 
-        num_batches = len(training_set)
-        sampler = IndexedDataLoaderSampler(num_batches, batch_size, 1)
-        training_data_loader = DataLoader(training_set, batch_size=batch_size, sampler=sampler)
+        training_data_loader = DataLoader(training_set, batch_size=batch_size)
 
         for callback in callbacks:
             callback.on_training_begin(len(training_data_loader), True)
 
-        # if self._random_state_initializers is not None:
-        #     torch.set_rng_state(self._random_state_initializers['last_batch_torch_random_state'])
-
-        # logs = {'last_batch_torch_random_state': torch.get_rng_state()}
         logs = {}
         for epoch in range(self._initial_epoch, epochs):
             for callback in callbacks:
                 callback.on_train_epoch_begin(epoch, True)
 
-            for bt, (x, y) in enumerate(training_data_loader):
-                batch = self._initial_batch + bt
-
+            for batch, (x, y) in enumerate(training_data_loader):
                 # Random state initializations if training a loaded model.
-                if self._random_state_initializers is not None:
-                    # In the first iteration over batches in a data loader, an iterable object
-                    # is created and it changes the random state in its constructor.
-                    # That's why we need to keep track of the random state at the end of the previous batch
-                    # and here we need to set the right random state when the model was saved.
-                    random.setstate(self._random_state_initializers['random_state'])
-                    np.random.set_state(self._random_state_initializers['numpy_random_state'])
-                    torch.set_rng_state(self._random_state_initializers['torch_random_state'])
-                    self._random_state_initializers = None
+                # if self._random_state_initializers is not None:
+                #     # In the first iteration over batches in a data loader, an iterable object
+                #     # is created and it changes the random state in its constructor.
+                #     # That's why we need to keep track of the random state at the end of the previous batch
+                #     # and here we need to set the right random state when the model was saved.
+                #     random.setstate(self._random_state_initializers['random_state'])
+                #     np.random.set_state(self._random_state_initializers['numpy_random_state'])
+                #     torch.set_rng_state(self._random_state_initializers['torch_random_state'])
+                #     self._random_state_initializers = None
 
                 for callback in callbacks:
                     callback.on_train_batch_begin(batch, True)
@@ -122,8 +88,8 @@ class ModelRunner:
 
                 # logs['last_batch_torch_random_state'] = torch.get_rng_state()
 
-            self._initial_batch = 0
-            training_data_loader.sampler = IndexedDataLoaderSampler(num_batches, batch_size, 0)
+            # self._initial_batch = 0
+            # training_data_loader.sampler = IndexedDataLoaderSampler(num_batches, batch_size, 0)
 
             for callback in callbacks:
                 callback.on_train_epoch_end(epoch, logs, True)
